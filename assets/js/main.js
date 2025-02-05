e85d619434ea877726ce0f0e69ad33deda1de872
GuilhermeAilton <ailtongui289@gmail.com>
2025-02-05 19:01:54 -0300

    Adicinando section do Regulamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,6 +104,20 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
+
+  var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
 // COROUSEL INIFINITO DE PATROCINADORES
 var copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logos").appendChild(copy);
